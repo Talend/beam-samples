122727d3d82ebf3811494d5706f1db9f9a3b1102
Jean-Baptiste Onofré <jbonofre@apache.org>
2016-06-03 15:00:04 +0200

    Initial revision

---
+++ DailyEvent2Kafka/src/main/java/org/apache/beam/samples/EventToKafkaPipeline.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.samples;
+
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.io.kafka.KafkaIO;
+import org.apache.beam.sdk.options.*;
+import org.apache.beam.sdk.repackaged.com.google.common.collect.ImmutableList;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class EventToKafkaPipeline {
+
+    /**
+     * Specific pipeline options.
+     */
+    private static interface Options extends PipelineOptions {
+
+        @Description("GDELT file prefix (basically date)")
+        String getFilePrefix();
+        void setFilePrefix(String value);
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        Options options = PipelineOptionsFactory.fromArgs(args).withValidation().as(Options.class);
+        Pipeline pipeline = Pipeline.create(options);
+
+        String filePrefix = options.getFilePrefix();
+        if (filePrefix == null) {
+            SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+            filePrefix = format.format(new Date());
+        }
+
+        pipeline
+                .apply(TextIO.Read.named("GDELT Daily File").from("http://data.gdeltproject.org/events/" + filePrefix + ".export.CSV.zip").withCompressionType(TextIO.CompressionType.ZIP))
+                .apply(KafkaIO.write().withBootstrapServers("localhost:9092").withTopics(ImmutableList.of("gdelt")));
+
+        pipeline.run();
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# Apache Beam samples
+
+This project is composed by several samples. The purpose is to download and analyze GDELT Project data
+using Apache Beam pipelines.
+
+The objectives are:
+
+1. Show how to implement Apache Beam pipelines for both streaming and batch analyses.
+2. Show how to run those pipelines on several runners.
+
+GDELT Project stores all news articles as "events": http://data.gdeltproject.org/events/index.html
+
+Daily, a zip file is created, containing a CSV file with all events using the following format:
+
+----
+545037848       20150530        201505  2015    2015.4110                                                                                       JPN     TOKYO   JPN                                                             1       046     046     04      1       7.0     15      1       15      -1.06163552535792       0                                                       4       Tokyo, Tokyo, Japan     JA      JA40    35.685  139.751 -246227 4       Tokyo, Tokyo, Japan     JA      JA40    35.685  139.751 -246227 20160529        http://deadline.com/print-article/1201764227/
+----
+
+The format is described: http://data.gdeltproject.org/documentation/GDELT-Data_Format_Codebook.pdf
