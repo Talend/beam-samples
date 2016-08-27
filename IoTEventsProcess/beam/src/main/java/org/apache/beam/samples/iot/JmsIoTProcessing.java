0d1b28d40137a055e56497a5a686c34ca7cd9a34
Jean-Baptiste Onofré <jbonofre@apache.org>
2016-08-27 09:13:40 +0200

    Add IoT JMS pipeline

---
+++ IoTEventsProcess/beam/src/main/java/org/apache/beam/samples/iot/JmsIoTProcessing.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.samples.iot;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.io.jms.JmsIO;
+import org.apache.beam.sdk.io.jms.JmsRecord;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.transforms.SimpleFunction;
+
+import javax.jms.ConnectionFactory;
+
+public class JmsIoTProcessing {
+
+    public static void main(String[] args) throws Exception {
+        Pipeline pipeline = Pipeline.create(PipelineOptionsFactory.create());
+
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+
+        pipeline.apply(JmsIO.read().withConnectionFactory(connectionFactory).withQueue("iot"))
+            .apply(MapElements.via(new SimpleFunction<JmsRecord, String>() {
+                public String apply(JmsRecord input) {
+                    return input.getPayload();
+                }
+            }))
+            .apply(TextIO.Write.to("/tmp/test"));
+
+        pipeline.run();
+    }
+
+}
