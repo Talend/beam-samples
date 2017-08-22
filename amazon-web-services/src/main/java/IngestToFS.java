107bd9f19dcc5addf99d82164fceead290d49313
Ismaël Mejía <iemejia@gmail.com>
2017-08-22 16:52:37 +0200

    Add sample of reading from AWS S3 via Hadoop File System

---
+++ amazon-web-services/src/main/java/IngestToFS.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.samples.ingest;
+
+import java.util.List;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.io.hdfs.HadoopFileSystemOptions;
+import org.apache.beam.sdk.options.Default;
+import org.apache.beam.sdk.options.DefaultValueFactory;
+import org.apache.beam.sdk.options.Description;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class IngestToFS {
+
+  private static final Logger LOG = LoggerFactory.getLogger(IngestToFS.class);
+
+  /**
+   * Specific pipeline options.
+   */
+//  private interface Options extends PipelineOptions {
+   private interface Options extends HadoopFileSystemOptions {
+
+    String GDELT_EVENTS_URL = "s3a://gdelt-open-data/events/";
+
+    @Description("GDELT file date") @Default.InstanceFactory(Options.GDELTFileFactory.class) String getDate();
+    void setDate(String value);
+
+    @Description("Input Path") String getInput();
+    void setInput(String value);
+
+    @Description("Output Path")
+    String getOutput();
+    void setOutput(String value);
+
+    class GDELTFileFactory implements DefaultValueFactory<String> {
+
+      public String create(PipelineOptions options) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+        return format.format(new Date());
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    Options options = PipelineOptionsFactory.fromArgs(args).withValidation().as(Options.class);
+    if (options.getInput() == null) {
+      options.setInput(Options.GDELT_EVENTS_URL + options.getDate() + ".export.csv");
+    }
+    options.setOutput("/tmp/beam/ingesttofs");
+    LOG.info(options.toString());
+    System.out.println(options.toString());
+
+    Pipeline pipeline = Pipeline.create(options);
+//    pipeline.apply("ReadFromGDELTFile", TextIO.read().from(options.getInput()))
+    pipeline.apply("ReadFromGDELTFile", TextIO.read().from(options.getHdfsConfiguration().get(0).get("fs.default.name")))
+        .apply("WriteToFS", TextIO.write().to(options.getOutput()));
+    pipeline.run();
+  }
+}
